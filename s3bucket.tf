8eb1fc5880474ec6c17deba83241fdfa566e78e1
nandkishor12 <nandkishor.wankhede23@gmail.com>
2025-04-23 17:05:25 +0530

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "ap-south-1"
-FILE_NAME = 's3bucket.tf'
+
 }
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "nandkishor-999"  # Yeh naam globally unique hona chahiye
